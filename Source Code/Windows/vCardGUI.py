0fd8dfe4dfbe09f68a8a5d2da16203372ec068ad
Minoxs <gufrgs2018@gmail.com>
2020-03-26 21:36:32 -0300

    vCardMaker 3.0 (Multi-Option) finished!

--- Source Code/Windows/vCardGUI.py
+++ Source Code/Windows/vCardGUI.py
@@ -2,21 +2,22 @@
 import PySimpleGUI as gui
 from vcardmaker import *
 
+#change colour of GUI's background
 gui.change_look_and_feel('Dark Blue 3')
 
 layout = [
 	[gui.T("Agora é possível selecionar vários emails de uma vez!")],
 	[gui.T("O Nome do vCard será definido pelo arquivo da lista de emails!")],
-	[gui.T("Arquivo de Emails",size=(15,1)),gui.In(size=(70,2),key="file"),gui.FilesBrowse("Procurar",file_types=(("Arquivo de Texto","*.txt"),)),gui.T(" "*13),gui.Button("Sair")],
+	[gui.T("Arquivo de Emails",size=(15,1)),gui.In(size=(70,2),key="file"),gui.FilesBrowse("Procurar",file_types=(("Arquivo de Texto","*.txt"),))],
 	[gui.T("Salvar em:",size=(15,1)),gui.In(size=(70,2),key="save"),gui.FolderBrowse("Procurar")],
 	[gui.Checkbox("Checar existencia de emails duplicados", key="dupe_check")],
 	[gui.Checkbox("Criar lista adicional única com todos emails", key="group_emails_check")],
 	[gui.Output(size=(112,20))],
 	[gui.Button("Confirmar",size=(100,2))],
-	[gui.T("Versão 3.0", text_color="dark gray")]
+	[gui.T("Versão 3.0", text_color="dark gray"), gui.Button("Sair",size=(10,1))]
 ]
 
-create_save_folder()
+create_save_folder() #creates save folder if one doesn't exist already
 window = gui.Window("vCardMaker",layout)
 
 while True:
@@ -24,10 +25,12 @@ while True:
 	if event in (None,"Sair"):
 		break
 	if event == "Confirmar":
+		#gets input values
 		get_file = values['file']
 		get_save_folder = values['save']
 		checkForDuplicate = values['dupe_check']
 		doCreateAllEmailCard = values['group_emails_check']
+		#if anything is empty, throws a soft error
 		if get_file.strip() == "":
 			print("Selecione o arquivo .txt de emails.")
 			continue
@@ -35,10 +38,12 @@ while True:
 			print("Caminho para salvar não especificado.")
 			continue
 
+		#get_file is a giant string with filepaths separated by ;
+		#use string.split() to turn into a list of strings (of filepaths to email list)
 		all_files = get_file.split(";")
-		all_emails = [[],[]] #primeira lista guarda o grupo, a segunda guarda o email
+		all_emails = [[],[]] #all_emails[0] holds group name and #all_emails[1] holds emails
 		for file in all_files:
-			group_name = file[file.rfind("/")+1:file.find(".txt")]
+			group_name = file[file.rfind("/")+1:file.find(".txt")] #gets group name from the filepath itself
 			email_list = abrir(file)
 
 			if checkForDuplicate:
--- Source Code/Windows/vcardmaker.py
+++ Source Code/Windows/vcardmaker.py
@@ -1,15 +1,13 @@
-#!/usr/bin/env python3
-lb = "\n"
 from pathlib import Path as find
-from collections import Counter
-error_msg = "Something went Wrong!"
 
+#function that prints to a file with extension .vcf
 def printer(add_to_log,save_location, extension = ".vcf"):
 	path = save_location+extension
 	p = open(path, "a+")
-	p.write(str(add_to_log) + lb)
+	p.write(str(add_to_log) + "\n")
 	p.close()
 
+#unpacks .txt into a list containing all emails
 def abrir(path_to_txt):
 	emails = []
 	try:
@@ -21,55 +19,54 @@ def abrir(path_to_txt):
 		if len(emails) == 0:
 			print("Arquivo de emails vazio!")
 			return 0
-		#dup_test = Counter(emails)
-		#print(dup_test)
-		#for i in dup_test:
-		#	if dup_test[i] > 1:
-		#		for j in range(dup_test[i]-1):
-		#			emails.remove(i)
 	except:
 		print(150*"\n")
 		print("Arquivo Inválido!")
 		return 0
 	return emails
 
+#function to create a default save folder if one doesn't exist already
 def create_save_folder():
-	find(str(find.cwd())+"/Save").mkdir(parents=True,exist_ok=True)
+	find(str(find.cwd())+"/Save").mkdir(parents=True,exist_ok=True) 
 
 def check_for_duplicate(email_list, emails_to_add, group_name):
 	for new_email in emails_to_add:
 		for i in range(len(email_list[1])):
 			if new_email == email_list[1][i]:
+				print("Duplicações devem ser resolvidas manualmente")
 				print("Email {} Já existe".format(new_email))
 				print("Grupos {} e {}".format(email_list[0][i], group_name))
 		email_list[0].append(group_name) #em email_list[0] estão o nome do grupo
 		email_list[1].append(new_email)	 #em email_list[1] está o email da pessoa
 	return email_list
 
+#function to generate vCards
 def generate_vcard(emails,save_path,group_name):
 	vcard_filename = "contatos_{}".format(group_name)
 	save_location = "{}/{}".format(save_path,vcard_filename)
 
+	#vCard naming portion
 	j = 1
-
 	temp = save_location
 	while find(save_location+".vcf").exists():
 		save_location = "{}({})".format(temp,j)
 		j += 1
 	del temp
-
+	#if vCard exists, adds (n) to the end of name, where n is how many vCards with that name exists
 	if j > 1:
 		print(2*"\n")
 		print("Arquivo vCard já existe!")
 		print("Novo nome do vCard: {}({})".format(vcard_filename,j-1))
 		print(2*"\n")
 
+	#vCard creation portion
 	check = 0
 	prob = 0
 	for i in range(len(emails)):
+		#error check portion of email
 		if emails[i].strip() == "":
 			continue
-		if emails[i].count("@") > 1:
+		elif emails[i].count("@") > 1:
 			print("\n")
 			print("ERRO!")
 			print("Há mais de um email na linha número {}".format(i+1))
@@ -78,7 +75,7 @@ def generate_vcard(emails,save_path,group_name):
 			printer(emails[i],save_location+"_problemas",".txt")
 			prob += 1
 			continue
-		if emails[i].count("@") < 1:
+		elif emails[i].count("@") < 1:
 			print("\n")
 			print("ERRO!")
 			print("Não há um email válido na linha número {}".format(i+1))
@@ -87,6 +84,7 @@ def generate_vcard(emails,save_path,group_name):
 			printer(emails[i],save_location+"_problemas",".txt")
 			prob += 1
 			continue
+		#vCard is being created here
 		printer("BEGIN:VCARD",save_location)
 		printer("VERSION:3.0",save_location)
 		if group_name.strip() != "":
@@ -96,7 +94,7 @@ def generate_vcard(emails,save_path,group_name):
 		printer("EMAIL;type=INTERNET:{}".format(emails[i]),save_location)
 		printer("END:VCARD",save_location)
 		check += 1
-
+	#creates new list of emails without any problems
 	if prob >= 1:
 		for i in range(len(emails)):
 			if emails[i].count("@") == 1:
